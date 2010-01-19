e18c7f6a9d0fbd0c9ef2b45dd3ebd555cef91d3a
Sean Brant <brant.sean@gmail.com>
2010-01-18 18:17:33 -0600

    intial files

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='Pylicious',
+      version='0.1',
+      description='Python Delicious API Client. Supports both HTTP and OAUTH authentication.',
+      author='Sean Brant',
+      py_modules=['pylicious'])
