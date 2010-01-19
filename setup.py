c1e13d199de52230458b2f7154acc62bbe126557
Sean Brant <brant.sean@gmail.com>
2010-01-18 18:37:31 -0600

    added requires oauth to setup

--- setup.py
+++ setup.py
@@ -4,4 +4,6 @@ setup(name='Pylicious',
       version='0.1',
       description='Python Delicious API Client. Supports both HTTP and OAUTH authentication.',
       author='Sean Brant',
-      py_modules=['pylicious'])
+      py_modules=['pylicious'],
+      requires=['oauth'],
+)
